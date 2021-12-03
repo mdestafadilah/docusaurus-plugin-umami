845dfa9c02265518cc9c763ffaa4d52f44d932b6
mdestafadilah <desta.08b@gmail.com>
2021-12-03 15:25:52 +0700

    LINTING

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
 import { HtmlTags, LoadContext, Plugin } from '@docusaurus/types';
 
-export default function umamiTagAnalytics({ siteConfig: { themeConfig }}: LoadContext): Plugin<void> {
-
+export default function umamiTagAnalytics({
+  siteConfig: { themeConfig },
+}: LoadContext): Plugin<void> {
   const { umamitag }: any = themeConfig || {}; // global
 
   const websiteId = umamitag?.websiteId;
   const src = umamitag?.src;
 
   if (!umamitag) {
-      throw new Error(
-        'You need to specify `umamitag` object in `themeConfig` '
-        + 'with `code` field in it to use docusaurus-plugin-umamitag'
-      )
+    throw new Error(
+      'You need to specify `umamitag` object in `themeConfig` ' +
+        'with `code` field in it to use docusaurus-plugin-umamitag'
+    );
   }
   if (!src) {
-      throw new Error(
-          'You specified the `umamitag` object in `themeConfig`, '
-          + 'but the `code` field was missing. '
-      )
+    throw new Error(
+      'You specified the `umamitag` object in `themeConfig`, ' +
+        'but the `code` field was missing. '
+    );
   }
- 
+
   if (!websiteId) {
     throw new Error(
       'You specified the `umamitag` object in `themeConfig` but the `websiteId` field was missing. ' +
@@ -36,21 +37,21 @@ export default function umamiTagAnalytics({ siteConfig: { themeConfig }}: LoadCo
 
   const analyticsDomain = `https://${src}/umami.js`;
 
-  const injectUmamiTag = () : { headTags: HtmlTags } => {
+  const injectUmamiTag = (): { headTags: HtmlTags } => {
     return {
       headTags: [
         {
           tagName: 'script',
           attributes: {
-              src: analyticsDomain,
-              'data-website-id': websiteId
-          }
-        }
-      ]
-    }
-  }
+            src: analyticsDomain,
+            'data-website-id': websiteId,
+          },
+        },
+      ],
+    };
+  };
   return {
     name: 'docusaurus-plugin-umami',
-    injectHtmlTags: isProd ? injectUmamiTag : undefined
-  }
+    injectHtmlTags: isProd ? injectUmamiTag : undefined,
+  };
 }
